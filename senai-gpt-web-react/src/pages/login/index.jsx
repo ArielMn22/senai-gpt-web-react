3e3301e86714f7048e0b38c7a1d2e259c8f86ecc
ArielMn22 <arielpaixao10@gmail.com>
2025-04-17 13:34:41 -0300

    feat: add SVG assets for chat, example, image, microphone, send, and React logo; implement global styles and login page

---
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -0,0 +1,61 @@
+import "./login.css";
+import logo from "../../assets/imgs/Chat.png";
+import { useState } from "react";
+
+function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onLoginClick = async () => {
+
+        let response = await fetch ("https://senai-gpt-api.azurewebsites.net/login", {
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: "POST", // Método que envia dados
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+
+        });
+
+        console.log(response);
+
+    }
+
+    return (
+        <>
+            <header></header>
+
+            <main className="page-container">
+
+                <div className="robo-image">
+                </div>
+
+                <div className="login-container">
+
+                    <img className="logo" src={logo} alt="Logo do SenaiGPT." />
+
+                    <h1
+                        id="meutitulo"
+                        className="titulo"
+                    >Login</h1>
+
+                    <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder="Insira o e-mail" />
+                    <input className="inpt" value={password} onChange={event => setPassword(event.target.value)} type="password" placeholder="Insira a senha" />
+
+                    <button className="btn" onClick={() => onLoginClick()}>Entrar</button>
+
+                </div>
+
+            </main>
+
+            <footer></footer>
+        </>
+    )
+}
+
+export default Login;
